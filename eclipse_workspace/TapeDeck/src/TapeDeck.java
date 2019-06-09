dba97ba8e1f1c9730e033dd44a341d07480b3efb
SENO <SENO@192.168.0.9>
2019-06-10 00:03:04 +0700

    3

--- eclipse_workspace/TapeDeck/src/TapeDeck.java
+++ eclipse_workspace/TapeDeck/src/TapeDeck.java
@@ -2,11 +2,7 @@
 public class TapeDeck {
 
 	boolean canRecord = false;
-<<<<<<< HEAD
 	int x = 3;
-=======
-	int x = 1;
->>>>>>> branch 'master' of https://github.com/senowidya/HeadFirstJava2ndEd.git
 	
 	void playTape() {
 		System.out.println("tape playing");
